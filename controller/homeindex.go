03e1e82f970def5e388b9227ca00a85dd608854c
engineer-yone3 <engineer.yone3@gmail.com>
2022-03-22 00:53:55 +0900

    JSONでリターンできた

--- controller/homeindex.go
+++ controller/homeindex.go
@@ -4,6 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Sample struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Comment string `json:"comment"`
+}
+
+
 func IndexAction(c *gin.Context) {
-	c.String(200, "Hello, World")
+	sample := Sample {
+		Id: 100,
+		Name: "テストなまえ",
+		Comment: "この人はクレーマーです",
+	}
+	// c.String(200, "Hello, World")
+	c.JSON(200, sample)
 }
